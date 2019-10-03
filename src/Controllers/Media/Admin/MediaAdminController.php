60b0c41d9ca99001e9592cf40567fc7db8840bc7
Jordan <jordan@daydream-lab.com>
2019-10-03 12:32:51 +0800

    update

--- src/Controllers/Media/Admin/MediaAdminController.php
+++ src/Controllers/Media/Admin/MediaAdminController.php
@@ -25,6 +25,7 @@ class MediaAdminController extends BaseController
 
     public function createFolder(MediaAdminCreateFolderPost $request)
     {
+        $this->service->canAction('createFolder');
         $this->service->createFolder($request->rulesInput());
 
         return ResponseHelper::response($this->service->status, $this->service->response);
@@ -33,6 +34,7 @@ class MediaAdminController extends BaseController
 
     public function getAllFolders()
     {
+        $this->service->canAction('getAllFolder');
         $this->service->getAllFolders();
 
         return ResponseHelper::response($this->service->status, $this->service->response);
@@ -41,6 +43,7 @@ class MediaAdminController extends BaseController
 
     public function getFolderItems(MediaAdminGetFolderItemsPost $request)
     {
+        $this->service->canAction('getFolderItems');
         $this->service->getFolderItems($request->rulesInput());
 
         return ResponseHelper::response($this->service->status, $this->service->response);
@@ -49,6 +52,7 @@ class MediaAdminController extends BaseController
 
     public function move(MediaAdminMovePost $request)
     {
+        $this->service->canAction('move');
         $this->service->move($request->rulesInput());
 
         return ResponseHelper::response($this->service->status, $this->service->response);
@@ -57,6 +61,7 @@ class MediaAdminController extends BaseController
 
     public function remove(MediaAdminDeletePost $request)
     {
+        $this->service->canAction('delete');
         $this->service->remove($request->rulesInput());
 
         return ResponseHelper::response($this->service->status, $this->service->response);
@@ -65,6 +70,7 @@ class MediaAdminController extends BaseController
 
     public function rename(MediaAdminRenamePost $request)
     {
+        $this->service->canAction('rename');
         $this->service->rename($request->rulesInput());
 
         return ResponseHelper::response($this->service->status, $this->service->response);
@@ -73,6 +79,7 @@ class MediaAdminController extends BaseController
 
     public function upload(MediaAdminUploadPost $request)
     {
+        $this->service->canAction('add');
         $this->service->upload($request->rulesInput());
 
         return ResponseHelper::response($this->service->status, $this->service->response);
--- src/routes/api.php
+++ src/routes/api.php
@@ -13,10 +13,10 @@
 
 Route::group(['middleware' => ['api'], 'prefix' => 'api'], function () {
 
-
     Route::group(['middleware' => ['auth:api', 'expired', 'admin'], 'prefix' => 'admin'], function (){
 
         Route::group(['prefix' => 'media'], function (){
+
             Route::post('upload', 'DaydreamLab\Media\Controllers\Media\Admin\MediaAdminController@upload');
             Route::post('move', 'DaydreamLab\Media\Controllers\Media\Admin\MediaAdminController@move');
             Route::post('folder/items', 'DaydreamLab\Media\Controllers\Media\Admin\MediaAdminController@getFolderItems');
@@ -24,7 +24,6 @@ Route::group(['middleware' => ['api'], 'prefix' => 'api'], function () {
             Route::get('folder/all', 'DaydreamLab\Media\Controllers\Media\Admin\MediaAdminController@getAllFolders');
             Route::post('remove', 'DaydreamLab\Media\Controllers\Media\Admin\MediaAdminController@remove');
             Route::post('rename', 'DaydreamLab\Media\Controllers\Media\Admin\MediaAdminController@rename');
-
         });
     });
 });
