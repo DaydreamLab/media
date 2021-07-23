b9ec47a524f29299c9bdb15b80dcd283cda3d936
Jordan <ppuff1988@gmail.com>
2021-07-23 18:09:02 +0800

    temp

--- src/Requests/File/Admin/FileAdminSearchPost.php
+++ src/Requests/File/Admin/FileAdminSearchPost.php
@@ -40,12 +40,23 @@ class FileAdminSearchPost extends ListRequest
     public function validated()
     {
         $validated = parent::validated();
-        if ( $brand_id = $validated->get('brand_id') ) {
-            $validated['q'] = $this->q->whereHas('brands', function ($query) use ($brand_id) {
+
+        $q = $validated->get('q');
+        if ($brand_id = $validated->get('brand_id') ) {
+            $q = $q->whereHas('brands', function ($query) use ($brand_id) {
                 $query->where('brands_files_maps.brand_id', '=', $brand_id);
             });
         }
         $validated->forget('brand_id');
+
+        # 過濾可觀看的品牌
+        if (!$this->user()->isSuperUser && $this->user()->isAdmin) {
+            $q = $q->whereHas('brands', function ($q) {
+                $q->whereIn('brands_files_maps.brand_id', $this->user()->brands->pluck('id'));
+            });
+        }
+
+        $validated->put('q', $q);
 
         return $validated;
     }
