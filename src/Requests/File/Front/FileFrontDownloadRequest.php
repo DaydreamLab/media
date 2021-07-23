24d0cea25b816a505400daf1d5a8f8e3ab588c0a
Jordan <ppuff1988@gmail.com>
2021-07-23 18:09:36 +0800

    fix: 加入密碼下載

---
+++ src/Requests/File/Front/FileFrontDownloadRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace DaydreamLab\Media\Requests\File\Front;
+
+use DaydreamLab\JJAJ\Requests\AdminRequest;
+
+class FileFrontDownloadRequest extends AdminRequest
+{
+    protected $package = 'Media';
+
+    protected $modelName = 'File';
+
+    protected $apiMethod = 'getFile';
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return parent::authorize();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'password'  => 'nullable|string'
+        ];
+    }
+}
